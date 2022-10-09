0fe8185eb23ac933bbdb5f2ff030047859f0eb47
Abranlincoln <93231734+Abranlincoln@users.noreply.github.com>
2022-10-09 18:09:24 -0300

    Update and rename Atv5.java to ATV5.java

--- ATV5.java
+++ ATV5.java
@@ -1,32 +1,34 @@
 import java.util.*;
 
-public class ATV5 {
-
+public class ATV5 {    
     public static void main(String[] args) {
         Queue<Character> U = new LinkedList<>();
         Queue<Character> L = new LinkedList<>();
-    
-        Scanner Letras = new Scanner(System.in);
-        System.out.printf("Digite letras minusculas e maiusculas: ");
-       
-        String UandL = Letras.nextLine();
-       
-        for (int S = 0;S < UandL.length();S++){
-            char var_char = UandL.charAt(S);
+
+        try (Scanner Letter = new Scanner(System.in)) {
+            System.out.printf("Digite letras minusculas e maiusculas: ");
+   
+            String UandL = Letter.nextLine();
+   
+            for (int S = 0;S < UandL.length();S++){
+                char var_char = UandL.charAt(S);
                 if (Character.isUpperCase(var_char)) {
-                   U.add(var_char);
+                    U.add(var_char);
                 } 
                 else if (Character.isLowerCase(var_char)) {
-                   L.add(var_char);
+                    L.add(var_char);
                 }
+            }
         }
         System.out.println("Maiúsculas: ");
         while (!U.isEmpty()){
         System.out.print(U.poll());
+    
         }
         System.out.println("\nMinúsculas: ");
         while (!L.isEmpty()){
         System.out.print(L.poll());
+
         }
     }
 }
