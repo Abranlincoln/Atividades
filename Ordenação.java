c3a49564f2beee72aabd9eb3d01964099a339516
Abranlincoln <93231734+Abranlincoln@users.noreply.github.com>
2022-10-17 21:24:17 -0300

    Update Ordenação.java

--- Ordenação.java
+++ Ordenação.java
@@ -11,8 +11,8 @@ public class Ordenação {
             vetor[j + 1] = aux;
             System.out.println("Insert " + aux);
         }
-        
     }
+
     public static void insertionSort1(int[] vetor) {
         int i, j, aux1;
         for (i = 1; i < vetor.length; i++) {
@@ -23,7 +23,6 @@ public class Ordenação {
             vetor[j + 1] = aux1;
             System.out.println("Insert1 " + aux1);
         }
-        
     }      
 
     public static void mergeSort1(int[] vetor1, int inicio1, int fim1) {
@@ -33,8 +32,7 @@ public class Ordenação {
             mergeSort1(vetor1, meio1 + 1, fim1);
             merge(vetor1, inicio1, meio1, fim1);
             System.out.println("Merge1 " + (inicio1+meio1+fim1));
-        }
-        
+        } 
     }
 
     public static void mergeSort(int[] vetor, int inicio, int fim) {
@@ -45,7 +43,6 @@ public class Ordenação {
             merge(vetor, inicio, meio, fim);
             System.out.println("Merge " + (inicio+meio+fim));
         }
-        
     }
 
     public static void merge(int[] vetor, int inicio, int meio, int fim) {
@@ -72,19 +69,18 @@ public class Ordenação {
             i++;
         }
     }
-   
+
     public static void main(String[] args) {
         Random randomic = new Random();
         int inicio = 0;
         int fim = 5;
         int[] vetor1 = randomic.ints(20, 0, 20).toArray();
-        int[] vetor2 = randomic.ints(20000, 0, 20000).toArray();
+        int[] vetor = randomic.ints(20000, 0, 20000).toArray();
        
-        insertionSort(vetor2);
-        mergeSort(vetor2, inicio, fim);
         insertionSort1(vetor1);
         mergeSort1(vetor1, inicio, fim);
+        
+        insertionSort(vetor);
+        mergeSort(vetor, inicio, fim);
     }
 }
-
- 
