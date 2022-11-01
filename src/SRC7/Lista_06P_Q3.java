7016d4e160c5bb2c3e1f93dfbe634886d1b30e6d
Abranlincoln <abranlincgtgt@gmail.com>
2022-10-31 22:16:06 -0300

    Create Lista_06P_Q3.java

---
+++ src/SRC7/Lista_06P_Q3.java
@@ -0,0 +1,16 @@
+package SRC7;  
+
+// mergeSort: tipo de ordenação que utiliza a técnica de divisão e conquista, dividindo o vetor em subvetores até que cada um deles tenha apenas um elemento, e depois os mescla de forma ordenada.
+// O mergeSort é recomendado quando o vetor é grande, pois ele é mais eficiente que os outros algoritmos de ordenação.
+
+// insertionSort: tipo de ordenação que percorre o vetor e compara o elemento atual com o anterior, se o elemento atual for menor que o anterior, ele é inserido na posição correta.
+// O insertionSort é como se fosse uma carta de baralho, onde você vai pegando uma carta e colocando na posição correta, e assim vai fazendo com todas as cartas.
+
+// selectionSort: tipo de ordenação que percorre o vetor e compara o elemento atual com o restante do vetor, se o elemento atual for menor que o restante do vetor, ele é inserido na posição correta.
+// O selectionSort é um algoritmo de ordenação simples, mas não é recomendado quando o vetor é grande, pois ele é menos eficiente que os outros algoritmos de ordenação.
+
+// quickSort: tipo de ordenação que utiliza a técnica de divisão e conquista, escolhendo um elemento do vetor como pivô, e percorrendo o vetor comparando os elementos com o pivô, se o elemento for menor que o pivô, ele é inserido na posição correta.
+// O quickSort é melhor aproveitado quando ele produz subvetores com tamanho próximo, uma vez que uma lista terá o tamanho da outra, e assim por diante.
+
+// bubbleSort: tipo de ordenação que percorre o vetor e compara o elemento atual com o próximo, se o elemento atual for maior que o próximo, eles são trocados de posição.
+// O bubbleSort é recomendado quando o vetor é pequeno, pois ele é um algoritimo de ordenação muito simples. Logo não é recomendado quando o vetor é grande, pois ele é menos eficiente que os outros algoritmos de ordenação.
